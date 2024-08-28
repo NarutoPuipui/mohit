4645f1bf5fb8b3b373231b0609778cb4ba3a68c1
NarutoPuipui <126446420+NarutoPuipui@users.noreply.github.com>
2024-08-28 22:57:18 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ import PulkitC from "@/components/PulkitC.jsx"
 
 export default function Home() {
   return (
-    <main className="bg-black flex-col min-h-screen items-center justify-center p-10 px-15">
+    <main className="bg-black flex-col min-h-screen max-w-screen items-center justify-center p-10 px-15">
       
       <Herosection/>
       <Education/>
